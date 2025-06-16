9efdd9f2f57e5fe573f74e8b56f1026e9fd252ab
quantopaul <127382788+quantopaul@users.noreply.github.com>
2025-06-17 02:03:02 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,5 @@
+i=int(input("Input a number: "))
+if(i%2==0):
+                print("The entered number is even")
+else:
+        print("The entered number is odd")
---
+++ Task2.py
@@ -0,0 +1,5 @@
+sum=0
+for i in range(1, 51):
+                sum+=i
+# print(f"The sum of all nos. from 1 to 50 = {sum}")
+print("The sum of all nos. from 1 to 50 =",sum)
